54d4fc30757bbc35c5af876b6cb450cda2f39ba4
Md Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-03-06 00:56:49 +0600

    finishing of module 38

---
+++ summary.js
@@ -0,0 +1,16 @@
+alert("hi mystrery recap here");
+confirm("are you single");
+prompt("name");
+
+//location.reload()
+//location.href = '';
+//location.hash =
+//http: //127.0.0.1:5500/shopping.html
+
+//history.back()
+//history.go()
+
+//cookies
+
+//sessionStorage vs localStorage
+//cookies vs sessionStorage vs localStorage
