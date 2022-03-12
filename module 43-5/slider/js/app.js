d9366de7051921fec5d164e814f52f912fb3f57f
Md Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-03-12 10:42:57 +0600

    adding

---
+++ module 43-5/slider/js/app.js
@@ -0,0 +1,20 @@
+const images = [
+    "images/pic-1.jpg",
+    "images/pic-2.jpg",
+    "images/pic-3.jpg",
+    "images/pic-4.jpg",
+    "images/pic-5.jpg",
+    "images/pic-6.jpg",
+    "images/pic-7.jpg",
+];
+let imgIndex = 0;
+
+const imgElement = document.getElementById("slider");
+setInterval(() => {
+    if (imgIndex >= images.length) {
+        imgIndex = 0;
+    }
+    const imgUrl = images[imgIndex];
+    imgElement.setAttribute("src", imgUrl);
+    imgIndex++;
+}, 1000);
